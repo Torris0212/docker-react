bffbd528fba5ff03962ad9240539da3111f99dd2
Torris0212 <torrisjin0212@gmail.com>
2023-10-18 19:26:37 -0500

    adjust tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.get('.App-link');
+  const linkElement = screen.getByClassName('App-link');
   expect(linkElement).toBeInTheDocument();
 });
